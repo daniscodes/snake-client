8a5c6a950b4a818a711b802b4622f189b387ac98
daniscodes <dan97@bell.net>
2022-08-03 15:22:56 -0400

    Created play.js and connected it to the server

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.2.53',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Connected to server");
+
+  conn.on('data', data => {
+      console.log(data);
+    });
+
+  })
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
